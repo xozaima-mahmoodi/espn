ff70f7d9500853507f98e82c6f2cad2df49a6d76
Your Name <you@example.com>
2019-08-29 23:13:00 +0430

    Edit and Delete Action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,12 @@
 class MessagesController < ApplicationController
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
+
   def index
     @messages = Message.all.order("created_at DESC")
   end
 
   def show
-    @message = Message.find(params[:id])
+    
   end
 
   def new
@@ -21,9 +23,30 @@ class MessagesController < ApplicationController
     end
   end
 
+  def edit
+    
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to message_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path
+  end
+
   private
 
   def message_params
     params.require(:message).permit(:title, :description)
+  end
+
+  def find_message
+    @message = Message.find(params[:id])
   end
 end
